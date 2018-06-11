849b68b34d6edaba05e96bd1b8f8daa61724cb1f
Amit Kumar Kashyap <amit_kashyap01@infosys.com>
2018-06-11 21:44:36 +1000

    Initial Commit

---
+++ src/main/java/com/springbootadmin/microservicesadmin/MicroservicesadminApplication.java
@@ -0,0 +1,12 @@
+package com.springbootadmin.microservicesadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesadminApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springbootadmin/microservicesadmin/MicroservicesadminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springbootadmin.microservicesadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroservicesadminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
