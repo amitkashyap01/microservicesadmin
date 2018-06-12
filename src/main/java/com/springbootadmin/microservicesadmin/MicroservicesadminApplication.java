0a46fb594cd3b1c52a158c1125418eca937384d5
Amit Kumar Kashyap <amit_kashyap01@infosys.com>
2018-06-12 23:59:18 +1000

    Updated the POM and logic in mail class

--- src/main/java/com/springbootadmin/microservicesadmin/MicroservicesadminApplication.java
+++ src/main/java/com/springbootadmin/microservicesadmin/MicroservicesadminApplication.java
@@ -2,11 +2,32 @@ package com.springbootadmin.microservicesadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@EnableAdminServer
 @SpringBootApplication
 public class MicroservicesadminApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesadminApplication.class, args);
+	}
+	
+	@Configuration
+	public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+			http.logout().logoutUrl("/logout");
+			http.csrf().disable();
+
+			http.authorizeRequests().antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**").permitAll();
+			http.authorizeRequests().antMatchers("/**").authenticated();
+
+			http.httpBasic();
+		}
 	}
 }
